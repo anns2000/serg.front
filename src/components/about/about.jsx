b0f64e1a702c43f156fb531e353680604356e9f8
rewan <84548592+rewan-hosny@users.noreply.github.com>
2023-04-08 03:23:29 +0200

    .

--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import './about.css'
-import owner from '../../images/team-1.jpg'
+import owner from '../../images/5.jpg'
 export default function About(){
     return(
         <div className="about-page">
@@ -11,35 +11,37 @@ export default function About(){
                 <div className = 'personal-data'>
                     <img src={owner} alt='owner'/>
                     <div className="card-about">
-                        <p>Prof. Aboul Ella Hassanien</p>
-                        <p>Founder and Chair of SRGE</p>
+                  
                         <p>Faculty of Computers and Artificial Intelligence</p>
                     </div>
                 </div>
                 <div className="about-data">
-                    <h1>Prof. Aboul Ella Hassanien</h1>
-                    <br/>
-                    <p><b>Aboul Ella Hassanien (Abo)</b> is a Professor at Cairo University, Faculty of Computers and Artificial Intelligence, IT Department and the chair of the technology 
-                    center of blind and visual impaired people. Abo received his B.Sc. with honours in 1986 and M.Sc degree in 1993, 
-                    both from Ain Shams University, Faculty of Science, Pure Mathematics and Computer Science Department, Cairo, Egypt.<br/> <br/> 
-                         On September 1998, he received his doctoral degree from the Department of Computer Science, 
-                         Graduate School of Science & Engineering, Tokyo Institute of Technology, Japan.<br/><br/>  He is a Full Professor at Cairo University,
-                          Faculty of Computer and Information, IT Department.<br/><br/> <b>Professor Aboul Ella</b> is the founder and chair the scientific reserach group in Egypt.<br/><br/> 
-                          He has authored/coauthored over 380 research publications in peer-reviewed reputed journals, book chapters and conference proceedings.
-                          <br/><br/>  He has served as the general chair, co-chair, program chair, program committee member of various international conferences and 
-                          reviewer for various international journals.<br/><br/>  Since 2004, he is actively involved as technical committee in the International 
-                          Association of Science and Technology for Development (IASTED) for Image Processing and Signal Processing.<br/><br/>  He has received the
-                           Best Young Researcher Award, 1990, Scientific Academic Research, Cairo, Egypt and received the excellence younger researcher awards
-                            from Kuwait University for the academic year 2003/2004, He has gain the JSPS fellowship, Tokyo Institute of Technology, Japan.<br/><br/> 
-                             He has guest edited many special issues for international scientific journals.<br/><br/>  He has directed many funded research projects.
-                             <br/><br/><b>Dr. Abo </b>was a member of the Interim Advisory Board committee of the International Rough Set Society. He is the author, editor 
-                             and co-editor for more than 37 books in the area of rough computing, computational intelligence, Intelligent Social networks, Data mining,
-                              and E-commerce. <br/><br/> <b>Dr. Abo </b>has been established the formal Egyptian rough Sets Society (ERS) and chair of the Egyptian Rough Set IRSS
-                               Chapter. <br/><br/> <b>Dr. Abo </b>is a collaborative researcher member of the Computational Intelligence Laboratory in the Department of Electrical
-                                and Computer Engineering at the University of Manitoba.<br/><br/>  Professor <b>Abo</b>  is the chair of the Computer Science and Information 
-                                Technology Division at the Egyptian Syndicate of Scientific Professions (ESSP).<br/><br/>  His research interests include, Computational 
-                                intelligence, medical image analysis, security, animal identification and multimedia data mining.</p>
-                </div>
+              
+                    <h1>The Scientific Research Group in Egypt</h1>
+                    <br></br>
+	
+
+<p  className="about-p">The Scientific Research Group in Egypt (SRGE) is one of the largest research schools in Egypt and the Arab world that conducts a range of research and community activities in the field of artificial intelligence and its applications across different domains. Its main objectives are:</p>
+<ul className="about-li">
+  <li>To facilitate and encourage collaboration among young Egyptian researchers and increase their contributions to academic research.</li>
+  <li>To explore the use of artificial intelligence to address challenging real-life problems across different scientific fields.</li>
+  <li>To foster scientific and technological excellence in a specific research area.</li>
+  <li>To integrate the research efforts of the scientific team to drive innovation in scientific, technological, and socio-economic trajectories that shape the future of AI and its applications.</li>
+  <li>To produce Master's and PhD graduates who are capable of conducting high-quality academic research, publishing in top academic journals, obtaining tenure-track faculty positions at leading research universities, and excelling as educators and academics.</li>
+</ul>
+<p className="about-p">SRGE conducts applied research in artificial intelligence, with research results being applied to a wide range of fields, including medicine, agriculture, animal studies, climate change, renewable energy, cybersecurity, and the Internet of Things. The group covers a range of disciplines, including:</p>
+<ul  className="about-li">
+  <li>AI and applications</li>
+  <li>Computational biology</li>
+  <li>Metaverses</li>
+  <li>Climate change</li>
+  <li>Drug discovery</li>
+  <li>Operations research</li>
+  <li>Industrial Engineering</li>
+  <li>Computational Intelligence</li>
+</ul>
+                    </div>
+
             </div>
            
         </div>
